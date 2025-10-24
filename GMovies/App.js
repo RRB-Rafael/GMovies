7ecfd90ef2dce48670a283fc24978721a6acec51
RRB-Rafael <rafael.bastos9928@gmail.com>
2025-10-24 12:24:34 -0300

    ABULELE EU SOU MUITO BOM o Gallo por outro lado ta atumalakando

--- GMovies/App.js
+++ GMovies/App.js
@@ -1,17 +1,9 @@
 import 'react-native-gesture-handler';
-import AppNavigation from './navigation/appNavigation';
+import "./global.css";
+import AppNavigation from "./navigation/appNavigation";
 
 export default function App() {
   return (
     <AppNavigation />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- GMovies/navigation/appNavigation.js
+++ GMovies/navigation/appNavigation.js
@@ -1,5 +1,6 @@
+import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import HomeScreen from '../screens/HomeScreen';
 
 const Stack = createNativeStackNavigator();
@@ -8,11 +9,11 @@ export default function AppNavigation() {
   return (
     <NavigationContainer>
         <Stack.Navigator>
-            <Stack.Screen 
-            name='Home' 
-            options={{headerShown: false}} 
-            component={HomeScreen}/>
+            <Stack.Screen
+                name='Home'
+                options={{headerShown: false}}
+                component={HomeScreen} />
         </Stack.Navigator>
     </NavigationContainer>
   )
-} 
+}
